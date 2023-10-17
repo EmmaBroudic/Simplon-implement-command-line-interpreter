0ba3b718a047a9aa39720b57de5c368e18854cb8
unknown <emma.broudic35@gmail.com>
2023-10-17 09:03:46 +0200

    version corrigée

--- Cli.java
+++ Cli.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class Cli {
 
@@ -10,65 +11,52 @@ public class Cli {
 		Scanner scanner = new Scanner(System.in); // Listen to the standard input (console)
 		System.out.print("> "); // Prompt
 
-		// Date
-		LocalDate today = LocalDate.now();
-
-		// Time
-		LocalTime timeNow = LocalTime.now(); // Get time
-
-		//Date and time
-		String dateAndTime = today + "T" + timeNow;
-
-		// User
-                String name = System.getProperty("user.name");
-
-		// Home
-		String home = System.getProperty("user.home");
-
-		// OS
-		String operatingSysName = System.getProperty("os.name");
-		String operatingSysVersion = System.getProperty("os.version");
-		String operatingSys = operatingSysName + " (" + operatingSysVersion + ")";
-
 		while (true) { // Infinite loop
 			String command = scanner.nextLine(); // Get input from console as a string
 			String output = ""; // A variable named output of type String
 			String[] parts = command.split(" ");
+			int nbrElems = parts.length;
 
 			if (command.equals("exit")) {
 				break; // Forces exit of the while loop
 			} else {
 
 				if (command.equals("date")) {
-					System.out.println("The date is : " + today); // print date
+					output = "" + LocalDate.now(); // print date
 				} else if (command.equals("time")) {
-					System.out.println("The time is : " + timeNow); // print date
+					output = "" + LocalTime.now(); // print date
 				} else if (command.equals("datetime")) {
-					System.out.println("Date and time are : " + dateAndTime); // print date and time
+					output = "" + LocalDateTime.now(); // print date and time
 				} else if (command.equals("useraccount")) {
-					System.out.println("User account is : " + name); // print name
+					output = System.getProperty("user.name"); // print name
 				} else if (command.equals("userhome")) {
-					System.out.println("User home is : " + home); // print home
+					output = System.getProperty("user.home"); // print home
 				} else if (command.equals("os")) {
-					System.out.println("User os is : " + operatingSys); // print OS
-				} else {
-					if (command.startsWith("printenv")) {
-						System.out.println(parts[1]);
+					output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")"; // print OS
+				} else if (parts[0].equals("printenv")) {
+					if (nbrElems < 2) {
+						output = "";
+					} else {
 						String homeValue = System.getenv(parts[1]);
-						System.out.println(homeValue);
-        			} else if (command.startsWith("echo")) {						
-						int nbrLoop = parts.length;
+						if (homeValue == null) {
+							output = "";
+						} else {
+							output = homeValue;
+						}
+					}
+        			} else if (parts[0].equals("echo")) {
+					if (nbrElems < 2) {
+						output = "";
+					} else {						
 						String result = "";
 
-						for (int i = 1; i < nbrLoop; i++) {
+						for (int i = 1; i < nbrElems; i++) {
 							result = result + parts[i] + " ";
 						}
-
-						System.out.println(result);
-
-					} else {
-						output = "Command '" + command + "' not found.";
+						output = result;
 					}
+				} else {
+					output = "Command '" + command + "' not found.";
 				}
 			}
 			System.out.println(output); // Print with new line (ln)
